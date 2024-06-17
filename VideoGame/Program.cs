a85db4d362c9e0add57c3085abf0e4f284556bbf
abt93 <anthony.temmel@gmail.com>
2024-06-17 11:02:23 +0200

    Initialisation des deux personnages

--- VideoGame/Program.cs
+++ VideoGame/Program.cs
@@ -1,11 +1,22 @@
 ﻿using VideoGame;
+using static System.Formats.Asn1.AsnWriter;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Character personnage1 = new Character();
-        Character personnage2 = new Character();
+        Character goodGuy = new Character();
+        Character badGuy = new Character();
+
+        goodGuy.name = "Goku";
+        goodGuy.lifePoints = 10;
+        goodGuy.attackForce = 9001;
+        goodGuy.defenseForce = 9000;
+
+        badGuy.name = "Végéta";
+        badGuy.lifePoints = 10;
+        badGuy.attackForce = 9001;
+        badGuy.defenseForce = 9000;
 
         // mise en scène
     }
